c0f04e2843ac3600e41dd1b675921983d982792b
Willian Anderson <willian01314551@gmail.com>
2026-04-08 18:00:24 -0300

    adicionar: template para db em mysql

---
+++ mysql_template/nouvelle.sql
@@ -0,0 +1,77 @@
+DROP DATABASE IF EXISTS nouvelle;
+CREATE DATABASE nouvelle;
+
+USE nouvelle;
+
+CREATE TABLE Sala (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    qtde_assentos INT
+);
+
+CREATE TABLE Filme (
+    id INT PRIMARY KEY,
+    nome VARCHAR(100),
+    duracao INT,
+    descricao TEXT
+);
+
+CREATE TABLE Usuario (
+    cpf VARCHAR(14) PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    senha VARCHAR(255),
+    telefone VARCHAR(20),
+    data_nasc DATE,
+    estudante BOOLEAN,
+    pcd BOOLEAN,
+    permissao ENUM('CLIENTE', 'ADMINISTRADOR') DEFAULT 'CLIENTE'
+);
+
+CREATE TABLE Pagamento (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    valor_total DECIMAL(10,2),
+    metodo_pagamento ENUM('PIX', 'CARTAO_CREDITO', 'CARTAO_DEBITO'),
+    status ENUM('PENDENTE', 'APROVADO', 'RECUSADO', 'ESTORNADO'),
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    fk_Usuario_cpf VARCHAR(14)
+);
+
+CREATE TABLE Ingresso (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_Pagamento_id INT,
+    fk_Filme_id INT
+);
+
+CREATE TABLE sessao (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_Sala_id INT,
+    fk_Filme_id INT,
+    horario_inicio DATETIME,
+    dub_leg ENUM('DUB', 'LEG')
+);
+
+ALTER TABLE Pagamento ADD CONSTRAINT FK_Pagamento_Usuario
+    FOREIGN KEY (fk_Usuario_cpf)
+    REFERENCES Usuario (cpf)
+    ON DELETE CASCADE;
+
+ALTER TABLE Ingresso ADD CONSTRAINT FK_Ingresso_Pagamento
+    FOREIGN KEY (fk_Pagamento_id)
+    REFERENCES Pagamento (id)
+    ON DELETE RESTRICT;
+
+ALTER TABLE Ingresso ADD CONSTRAINT FK_Ingresso_Filme
+    FOREIGN KEY (fk_Filme_id)
+    REFERENCES Filme (id)
+    ON DELETE CASCADE;
+
+ALTER TABLE sessao ADD CONSTRAINT FK_sessao_Sala
+    FOREIGN KEY (fk_Sala_id)
+    REFERENCES Sala (id)
+    ON DELETE SET NULL;
+
+ALTER TABLE sessao ADD CONSTRAINT FK_sessao_Filme
+    FOREIGN KEY (fk_Filme_id)
+    REFERENCES Filme (id)
+    ON DELETE SET NULL;
